10d69e9fcd91acfce038bc065e2c3d40e8bfa31d
xiao-dd <59915536+xiao-dd@users.noreply.github.com>
2023-01-22 14:47:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,5 +13,9 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+# 修改默认内核（所有机型都适用，只要您编译的机型源码附带了其他内核，请至编译说明的第12条查看https://github.com/danshui-git/shuoming/blob/master/%E4%BF%AE%E6%94%B9%E5%86%85%E6%A0%B8%E7%89%88%E6%9C%AC.md）
+sed -i 's/PATCHVER:=6.1/PATCHVER:=5.15/g' target/linux/x86/Makefile
+
+
 # Add a feed source
 sed -i '$a src-git smpackage https://github.com/kenzok78/small-package' feeds.conf.default
