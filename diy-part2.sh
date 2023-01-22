e131172667bb99acbf398e1925b2cef4477a6e42
xiao-dd <59915536+xiao-dd@users.noreply.github.com>
2023-01-22 14:41:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,7 +3,7 @@
 # Script to compile and create files for each openwrt
 # --------------------------------------------------------
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.1.252/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.98.2/g' package/base-files/files/bin/config_generate
 
 
 
